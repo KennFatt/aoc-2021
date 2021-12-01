b04ebe1fc284797aa74f976cf5cae19fd4905e61
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-12-01 23:32:20 +0700

    Change: inline the reducer fn

--- 01_sonar-sweep_pt-02/index.js
+++ 01_sonar-sweep_pt-02/index.js
@@ -10,10 +10,7 @@ const measurements = data
   .filter((_, idx) => idx < data.length - 2)
   .reduce((accu, _, idx) => {
     const arr = [data.slice(idx, idx + 3), data.slice(idx + 1, idx + 4)];
-
-    const sumFn = (p, c) => p + c;
-    const sum = arr.map((v) => v.reduce(sumFn, 0));
-
+    const sum = arr.map((v) => v.reduce((p, c) => p + c, 0));
     if (sum[1] > sum[0]) accu++;
 
     return accu;
